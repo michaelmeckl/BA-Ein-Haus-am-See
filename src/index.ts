4126a520a246f28b07fbee8f730f76dfe48b3962
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-06-01 19:17:54 +0200

    Add opn module to speed up development

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /* eslint-env node */
 import Server from "./server/server";
+import opn from "opn";
 
 const DEFAULT_PORT = 8000;
 
@@ -7,6 +8,9 @@ function init(): void {
   const appPort = Number(process.env.PORT || DEFAULT_PORT); // port to use for serving static files
   const server = new Server();
   server.start(appPort);
+
+  // open the website automatically
+  opn(`http://localhost:${appPort}`);
 }
 
 init();
