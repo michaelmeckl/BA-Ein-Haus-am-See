5cff749a6a0beb05dd190a7a4256607c33120fdb
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-06-30 20:51:46 +0200

    Implement overpass api request without wrapper
    This way every step can be measured for performance.

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,12 +4,43 @@ import path from "path";
 import cors from "cors";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import queryOverpass from "query-overpass";
-import { GeoJsonObject } from "geojson";
+import { performance } from "perf_hooks";
+import {
+  GeoJsonObject,
+  FeatureCollection,
+  GeometryObject,
+  Geometry,
+} from "geojson";
+import childProcess from "child_process";
+import os from "os";
+import Util from "util";
+import axios from "axios";
+//TODO: test
+import nodeFetch from "node-fetch";
+import needle from "needle";
+
+import osmtogeojson from "osmtogeojson";
+import querystring from "querystring";
+import xmldom from "xmldom";
+//import pg from "pg";
 
 const RESPONSE_OK = 200;
 const BAD_REQUEST = 400;
 const INTERNAL_SERVER_ERROR = 500;
+
+// TODO:
+/*
+const tileCoords = { x: 164, y: 396, z: 10 };
+
+const endpoint =
+  "https://api.maptiler.com/tiles/v3/{z}/{x}/{y}.pbf?key=mrAq6zQEFxOkanukNbGm";
+const tileURL = endpoint
+  .replace(/{z}/, tileCoords.z)
+  .replace(/{y}/, tileCoords.y)
+  .replace(/{x}/, tileCoords.x);
+  */
+
+const exec = Util.promisify(childProcess.exec);
 
 const staticDir = path.join(__dirname, "../", "public"); // folder with client files
 
@@ -32,6 +63,15 @@ export default class Server {
     const router = this.initRouter();
     this.app.use(router);
 
+    /*
+    // catch 404 and forward to error handler
+    this.app.use(function (req, res, next) {
+      const err: any = new Error("Not Found");
+      err.status = 404;
+      next(err);
+    });
+    */
+
     // use an application-level middleware to add the CORS HTTP header to every request by default.
     //this.app.use(cors());
 
@@ -43,6 +83,8 @@ export default class Server {
    * Start the express server on the given port.
    */
   start(port: number): void {
+    //this.executeScript("dir");
+
     this.app.listen(port, (err) => {
       if (err) {
         return console.error(err);
@@ -80,23 +122,27 @@ export default class Server {
           // TODO: show user some kind of progress information: progress bar or simply percentage / remaining time!
           //res.status(200).send("Got it! You sent: " + query + ",\n" + bounds);
 
-          const hrstart = process.hrtime();
+          //TODO: measure time
+          const osmQuery = this.buildOverpassQuery(bounds, query);
+          const geoData = await this.getDataFromOSM(osmQuery);
 
-          //TODO: das geht schöner (z.B. mit async und await oder promises!)
-          // is this called twice?
-          this.extractOSMData(bounds, query, (osmResponse, error) => {
-            const hrend = process.hrtime(hrstart);
-            console.log(`Execution time: ${hrend[1] / 1000000} ms`);
-
-            if (error) {
-              res.status(BAD_REQUEST).send(error);
-              return next(error);
-            }
-            return res.status(RESPONSE_OK).send(osmResponse);
-          });
+          if (geoData instanceof Error) {
+            console.log("instance of was error");
+            res.status(BAD_REQUEST).send(geoData);
+            return next(geoData);
+          }
+          console.log("sending to client");
+          return res.status(RESPONSE_OK).send(geoData);
         }
+        return res.end();
       }
     );
+
+    //TODO: fetch from db
+    //url: 'http://localhost:{port_of_db}/amenities',
+    router.get("/amenities", function (req, res) {
+      //this.getAmenities(req.body, res);
+    });
 
     return router;
   }
@@ -110,36 +156,23 @@ export default class Server {
     if (res.headersSent) {
       return next(err);
     }
+    //res.status(err.status || INTERNAL_SERVER_ERROR);
     res.status(INTERNAL_SERVER_ERROR);
     console.log(err);
     return res.send(err);
   }
 
   /*
-  async wait(ms) {
-    return new Promise((resolve, reject) => {
-      setTimeout(resolve, ms);
-    });
-  }*/
-
-  async extractOSMData(
-    bounds: string,
-    userQuery: string,
-    callback: (data?: GeoJsonObject, err?: Error) => void
-  ): Promise<void> {
-    const query = this.buildOverpassQuery(bounds, userQuery);
-    //console.log("Query: " + query);
-
-    // queryOverpass adds the ?data= automatically to the beginning of the query and uses "http://overpass-api.de/api/interpreter"
-    // as the baseUrl (the baseUrl can be changed however, see https://github.com/perliedman/query-overpass).
-    // For this reason, only the raw data query has to be provided as parameter.
-    queryOverpass(query, (err: Error, data: GeoJsonObject) => {
-      if (err) {
-        callback(undefined, err);
-      }
-      callback(data);
-    });
+  //TODO: use custom benchmarking class
+  // eslint-disable-next-line no-magic-numbers
+  measureTime(func: (args: any) => any, action: string, accuracy = 3): any {
+    const t0 = performance.now();
+    const val = func(args);
+    const t1 = performance.now();
+    console.log(`${action} took ${(t1 - t0).toFixed(accuracy)} milliseconds.`);
+    return val;
   }
+  */
 
   /**
    * Builds a query for the overpass api to fetch osm data as GeoJson in the given map bounds.
@@ -178,5 +211,120 @@ export default class Server {
     const query = `${querySettings}(${request});${output}`;
 
     return query;
+  }
+
+  async getDataFromOSM(query: string): Promise<any | Error> {
+    try {
+      const encodedQuery = querystring.stringify({ data: query });
+
+      //TODO: test performance for the others too
+      const response = await axios.get(
+        `https://overpass-api.de/api/interpreter?${encodedQuery}`
+      );
+
+      const response2 = await nodeFetch(
+        `https://overpass-api.de/api/interpreter?${encodedQuery}`
+      );
+      console.log(response2);
+      console.log(await response2.json());
+
+      const response3 = await needle(
+        "get",
+        `https://overpass-api.de/api/interpreter?${encodedQuery}`
+      );
+      console.log(response3);
+      console.log(response3.body);
+
+      const contentType = response.headers["content-type"];
+      console.log(contentType);
+
+      let data: any;
+      if (contentType.endsWith("json")) {
+        data = response.data as GeoJsonObject;
+      } else if (contentType.endsWith("xml")) {
+        const parser = new xmldom.DOMParser();
+        data = parser.parseFromString(response.data);
+      } else {
+        throw new Error("Content type not supported!");
+      }
+
+      const geoJson = osmtogeojson(data);
+      return geoJson;
+    } catch (error) {
+      console.log("Request failed: " + error);
+      return error;
+    }
+  }
+
+  //TODO: connect to and request data from postgres:
+  /*
+  getAmenities(amenities, res) {
+    const conString = "pg://localhost/portland_from_osm";
+    const client = new pg.Client(conString);
+    client.connect();
+
+    const fc = {
+      type: "FeatureCollection",
+      features: [],
+    };
+
+    const sql =
+      "SELECT name, ST_AsGeoJSON(ST_TRANSFORM(way, 4326)) AS way, tags->'cuisine' AS cuisine " +
+      "FROM planet_osm_point " +
+      "WHERE amenity = 'restaurant' AND name IS NOT NULL;";
+
+    client.query(sql, function (err, result) {
+      result.rows.forEach(function (feature) {
+        const tags = feature.tags;
+
+        const f = {
+          type: "Feature",
+          geometry: JSON.parse(feature.way),
+          properties: {
+            name: feature.name,
+            cuisine: feature.cuisine,
+          },
+        };
+        fc.features.push(f);
+      });
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.send(fc);
+    });
+  }
+  */
+
+  //TODO: test: execute command line scripts
+  async executeScript(script: string): Promise<void> {
+    // TODO: check for correct os!
+    const platform = this.getPlatform();
+    let exampleScript: string;
+
+    if (platform === "win32") {
+      exampleScript = "dir";
+    } else if (platform === "linux") {
+      exampleScript = "ls";
+    } else {
+      console.error("Only Windows and Linux are supported at the moment!");
+      return;
+    }
+
+    try {
+      const { stdout, stderr } = await exec(script);
+
+      // the *entire* stdout and stderr (buffered)
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+    } catch (error) {
+      // node couldn't execute the command
+      console.log("exec error: " + error);
+      return;
+    }
+  }
+
+  getPlatform(): string {
+    // Returns a string identifying the operating system platform. The value is set at compile time.
+    // Possible values are 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', and 'win32'.
+    return os.platform();
   }
 }
