a701ef25a4cfa0294f26a450c65877774469f75c
Michael Meckl <Michael.Meckl@stud.uni-regensburg.de>
2020-11-21 15:57:01 +0100

    Make the mapbox token public
    so the app can be tested without having to create a mapbox  account

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,6 +2,8 @@
  * Immutable configuration constants for client and server.
  */
 export const Config = {
+  PUBLIC_MAPBOX_TOKEN:
+    "pk.eyJ1IjoibWljaGFlbG1lY2tsIiwiYSI6ImNrYWNyMnd1bjA5aHAycnByamgzZHd6bTEifQ.33Midnjfp-CccC19KMMJSQ",
   SERVER_PORT: 8000,
   REDIS_PORT: 6379,
   OVERPASS_PORT: 12347,
